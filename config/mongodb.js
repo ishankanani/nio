f372622d3921f164f2429d4a1267f41df7813c56
Ishan Kanani <108275428+ishankanani@users.noreply.github.com>
2025-12-17 10:42:53 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("DB Connected");
+    });
 
-    mongoose.connection.on('connected',()=>{
-        console.log("DB Connected");
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
-}
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
